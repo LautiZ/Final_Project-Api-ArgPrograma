d6378b49dd7425629d84f80c321dbcc387dfa00b
LautiZ <zullolau@gmail.com>
2023-01-05 11:00:56 -0300

    Hash de contraseña y login(Comparacion del hash con contraseña ingresada).

--- src/main/java/com/cursojava/project/controllers/AuthController.java
+++ src/main/java/com/cursojava/project/controllers/AuthController.java
@@ -2,6 +2,8 @@ package com.cursojava.project.controllers;
 
 import com.cursojava.project.models.Usuario;
 import com.cursojava.project.repositories.UsuarioRepository;
+import com.cursojava.project.utils.JWTUtil;
+
 import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,9 @@ public class AuthController {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private JWTUtil jwtUtil;
+
     @PostMapping(value = "login")
     public String login(@RequestBody Usuario user) {
         List<Usuario> lista = usuarioRepository.findByUsername(user.getUsername());
@@ -26,7 +31,12 @@ public class AuthController {
             boolean correctPassword = argon2.verify(password, user.getPassword());
 
             if (correctPassword) {
-                return "OK";
+                String idUsuario = lista.get(0).getId().toString();
+                String emailUsuario = lista.get(0).getEmail();
+
+                String tokenUsuario = jwtUtil.create(idUsuario, emailUsuario);
+
+                return tokenUsuario;
             }
         }
 
--- src/main/java/com/cursojava/project/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/project/controllers/UsuarioController.java
@@ -3,19 +3,25 @@ package com.cursojava.project.controllers;
 import com.cursojava.project.models.Usuario;
 import com.cursojava.project.repositories.UsuarioRepository;
 
+import com.cursojava.project.utils.JWTUtil;
 import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
-import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @RestController
 public class UsuarioController {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private JWTUtil jwtUtil;
 
     // Ruta de prueba
     @RequestMapping(value = "prueba")
@@ -25,21 +31,51 @@ public class UsuarioController {
 
     // Ruta para ver todos los usuarios
     @GetMapping(value = "usuarios")
-    public List<Usuario> getPersona() {
+    public List<Usuario> getUsuarios(@RequestHeader(value = "Authorization") String token) {
+
+        if (!validarToken(token)) {
+            return null;
+        }
+
         return usuarioRepository.findAll();
     }
 
+    private boolean validarToken(String token) {
+        String idUsuario = jwtUtil.getKey(token);
+        return idUsuario != null;
+    }
+
     // Ruta para editar un usuario
-    @RequestMapping(value = "usuario/edit")
-    public Usuario editUser() {
-        Usuario user = new Usuario();
-        user.setNombre("Lautaro");
-        user.setApellido("Zullo");
-        user.setEmail("zullolau@gmail.com");
-        user.setTelefono(3512583767L);
-        user.setUsername("Lauti");
-        user.setPassword("156149356");
-        return user;
+    @PutMapping(value = "usuario/edit")
+    public Usuario editUser(@RequestHeader(value = "Authorization") String token, @RequestBody @NotNull Usuario usuario) {
+        if (!validarToken(token)) {
+            return null;
+        } else {
+            String id = jwtUtil.getKey(token);
+            Long idLong = Long.parseLong(id);
+
+            Usuario updatedUsuario = usuarioRepository.findById(idLong).get();
+
+            if (!Objects.equals(usuario.getNombre(), "")) {
+                updatedUsuario.setNombre(usuario.getNombre());
+            }
+            if (!Objects.equals(usuario.getApellido(), "")) {
+                updatedUsuario.setApellido(usuario.getApellido());
+            }
+            if (!Objects.equals(usuario.getEmail(), "")) {
+                updatedUsuario.setEmail(usuario.getEmail());
+            }
+            if (usuario.getTelefono() != null) {
+                updatedUsuario.setTelefono(usuario.getTelefono());
+            }
+            if (!Objects.equals(usuario.getUsername(), "")) {
+                updatedUsuario.setUsername(usuario.getUsername());
+            }
+
+            usuarioRepository.save(updatedUsuario);
+            return updatedUsuario;
+        }
+
     }
 
     // Ruta para eliminar un usuario
